a00624afbdc76267f6fd688e572ba3515e920e4d
Zeeshan-Arif <zeeshanarif01@gmail.com>
2022-11-24 16:26:20 +0530

    updated the views.py inside profile application to create modal and update the profile page

--- profiles/views.py
+++ profiles/views.py
@@ -6,10 +6,17 @@ from .forms import ProfileModelForm
 def my_profile_view(request):
     profile = Profile.objects.get(user=request.user)
     form = ProfileModelForm(request.POST or None, request.FILES or None, instance=profile)
+    confirm = False
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            confirm = True
 
     context = {
         'profile': profile,
         'form': form,
+        'confirm': confirm,
     }
 
     return render(request, 'profiles/myprofile.html', context)
