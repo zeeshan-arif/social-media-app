717e1914051a9c8424841989059965f3a3ff05da
Zeeshan-Arif <zeeshanarif01@gmail.com>
2022-11-26 22:36:32 +0530

    added like and unlike functionality to each post by a user

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import post_comment_create_and_list_view
+from .views import post_comment_create_and_list_view, like_unlike_post
 
 app_name = 'posts'
 
 urlpatterns = [
     path('', post_comment_create_and_list_view, name='main-post-view'),
+    path('liked/', like_unlike_post, name='like-post-view'),
+    #path('liked/', post_comment_create_and_list_view, name='main-post-view'),
 ]
